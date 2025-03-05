fdd534cccee8c4fbcedd990c2a7384d713499c7d
Fernando <fernandobarrosdesak@gmail.com>
2025-03-05 18:29:38 -0300

    implementando model, context, realizando migration.

--- EmprestimoLivros/EmprestimoLivros/Data/Context/ApplicationDbContext.cs
+++ EmprestimoLivros/EmprestimoLivros/Data/Context/ApplicationDbContext.cs
@@ -8,4 +8,13 @@ public class ApplicationDbContext : DbContext
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options){}
     
     public DbSet<EmprestimosModel>  Emprestimos { get; set; }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<EmprestimosModel>()
+            .Property(e => e.Id)
+            .HasDefaultValueSql("gen_random_uuid()");
+    }
+
 }
+
--- EmprestimoLivros/EmprestimoLivros/Models/EmprestimosModel.cs
+++ EmprestimoLivros/EmprestimoLivros/Models/EmprestimosModel.cs
@@ -1,10 +1,15 @@
-﻿namespace EmprestimoLivros.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmprestimoLivros.Models;
 
 public class EmprestimosModel
 {
-    public Guid id { get; set; }
+    public Guid Id { get; set; }
+    [Required(ErrorMessage = "Digite o nome do Recebedor")]
     public string Recebedor { get; set; }
+    [Required(ErrorMessage = "Digite o nome do Fornecedor")]
     public string Fornecedor { get; set; }
+    [Required(ErrorMessage = "Digite o nome do Livro")]
     public string Livro { get; set; }
-    public DateTime DataUltimaAtualizacao { get; set; } = DateTime.Now;
+    public DateTime DataUltimaAtualizacao { get; set; } = DateTime.UtcNow;
 }
