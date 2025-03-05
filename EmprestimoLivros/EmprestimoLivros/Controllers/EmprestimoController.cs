bda83eb963b4c9d4f602e2da61a690c070c256d4
Fernando <fernandobarrosdesak@gmail.com>
2025-03-05 18:30:23 -0300

    implementando novas views e controller

--- EmprestimoLivros/EmprestimoLivros/Controllers/EmprestimoController.cs
+++ EmprestimoLivros/EmprestimoLivros/Controllers/EmprestimoController.cs
@@ -17,4 +17,79 @@ public class EmprestimoController : Controller
         IEnumerable<EmprestimosModel> emprestimos = _context.Emprestimos;
         return View(emprestimos);
     }
+    
+    [HttpGet]
+    public IActionResult Cadastrar()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Cadastrar(EmprestimosModel emprestimo)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Emprestimos.Add(emprestimo);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        return View("Cadastrar");
+    }
+    
+    
+    [HttpGet]
+    public IActionResult Editar(Guid id)
+    {
+        if (id == null || id == Guid.Empty)
+            return NotFound();
+        
+        EmprestimosModel emprestimo = _context.Emprestimos.FirstOrDefault(e => e.Id == id);
+        
+        if (emprestimo == null)
+            return NotFound();
+        
+        return View(emprestimo);
+    }
+
+    [HttpPost]
+    public IActionResult Editar(EmprestimosModel emprestimo)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Emprestimos.Update(emprestimo);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        return View(emprestimo);
+    }
+
+    [HttpGet]
+    public IActionResult Excluir(Guid id)
+    {
+        if (id == null | id == Guid.Empty)
+            return NotFound();
+        
+        EmprestimosModel emprestimo = _context.Emprestimos.FirstOrDefault(x => x.Id == id);
+
+        if (emprestimo == null)
+            return NotFound();
+
+        return View(emprestimo);
+    }
+
+    [HttpPost]
+    public IActionResult Excluir(EmprestimosModel emprestimo)
+    {
+        if (emprestimo == null)
+            return NotFound();
+        
+        _context.Emprestimos.Remove(emprestimo);
+        _context.SaveChanges();
+
+        return RedirectToAction("Index");
+    }
 }
