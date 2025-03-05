dd81ff2b9ed82a491f690075d82b39ba4c156735
Fernando <fernandobarrosdesak@gmail.com>
2025-03-05 15:59:47 -0300

    criacao do model/tabela, gerando a migration e fazendo a criacao da tabela para exibir dados

---
+++ EmprestimoLivros/EmprestimoLivros/Data/Context/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using EmprestimoLivros.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmprestimoLivros.Data.Context;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options){}
+    
+    public DbSet<EmprestimosModel>  Emprestimos { get; set; }
+}
---
+++ EmprestimoLivros/EmprestimoLivros/Models/EmprestimosModel.cs
@@ -0,0 +1,10 @@
+﻿namespace EmprestimoLivros.Models;
+
+public class EmprestimosModel
+{
+    public Guid id { get; set; }
+    public string Recebedor { get; set; }
+    public string Fornecedor { get; set; }
+    public string Livro { get; set; }
+    public DateTime DataUltimaAtualizacao { get; set; } = DateTime.Now;
+}
